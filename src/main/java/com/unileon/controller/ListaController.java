4278b379e9b49c20685ef4ae66c403df8d604189
lredog00 <lredog00@estudiantes.unileon.es>
2020-05-29 16:36:56 +0200

    Estilo del registro usuario finalizado

--- src/main/java/com/unileon/controller/ListaController.java
+++ src/main/java/com/unileon/controller/ListaController.java
@@ -28,40 +28,41 @@ import javax.inject.Named;
  */
 @Named
 @ViewScoped
-public class ListaController implements Serializable{
+public class ListaController implements Serializable {
+
     @Inject
     private Lista lista;
-    
+
     @Inject
     private Persona persona;
     private List<Persona> listaDePersonas;
-    
+
     private List<ContenidoLista> contenidoLista;
-    
+
     @EJB
     private ContenidoListaFacadeLocal contenidoListaEJB;
-    
+
     @EJB
     private PersonaFacadeLocal personaEJB;
-    
+
     @EJB
     private ListaFacadeLocal listaEJB;
-        
+
     private List<Lista> arrayDeListas;
-    
+
     private Usuario usuario;
-    
+
     @Inject
     private ListasPersonalesController objetoLPC;
-    
+
     @PostConstruct
-    public void init(){
-        arrayDeListas= listaEJB.findAll();
+    public void init() {
+        arrayDeListas = listaEJB.findAll();
         listaDePersonas = personaEJB.findAll();
         usuario = (Usuario) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
         persona = usuario.getPersona();
         contenidoLista = contenidoListaEJB.findAll();
-        
+
     }
 
     public List<ContenidoLista> getContenidoLista() {
@@ -71,9 +72,7 @@ public class ListaController implements Serializable{
     public void setContenidoLista(List<ContenidoLista> contenidoLista) {
         this.contenidoLista = contenidoLista;
     }
-    
-    
-    
+
     public List<Persona> getListaDePersonas() {
         return listaDePersonas;
     }
@@ -113,45 +112,41 @@ public class ListaController implements Serializable{
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
-    
-    
-    public void crearLista(){
+
+    public void crearLista() {
         try {
-            
-                        
+
             lista.setPersona(persona);
             listaEJB.create(lista);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se creo la lista"));
-            
+
             objetoLPC.init();
         } catch (Exception e) {
-            
+
         }
     }
-    
-    public void eliminarLista(){
+
+    public void eliminarLista() {
         try {
-            for (Lista l:arrayDeListas) {
-                if(l.getIdLista() ==lista.getIdLista()){
-                    lista=l;
+            for (Lista l : arrayDeListas) {
+                if (l.getIdLista() == lista.getIdLista()) {
+                    lista = l;
                     break;
                 }
             }
-            
-            for(ContenidoLista cl:contenidoLista){
-                if(cl.getLista().getIdLista() == lista.getIdLista()){
+
+            for (ContenidoLista cl : contenidoLista) {
+                if (cl.getLista().getIdLista() == lista.getIdLista()) {
                     contenidoListaEJB.remove(cl);
                 }
             }
-            
+
             listaEJB.remove(lista);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se elimino la lista"));
             arrayDeListas = listaEJB.findAll();
         } catch (Exception e) {
-            System.out.println("Error al eliminar lista "+e.getMessage());
+            System.out.println("Error al eliminar lista " + e.getMessage());
         }
     }
-   
-    
-    
+
 }
